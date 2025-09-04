2776da3a0d71137e51004f3744842e556d53eeca
bijoy <bijoypjoseph07@gmail.com>
2025-09-04 13:23:23 +0530

    almost complete transition to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Head from "next/head";
-import { gsap } from "gsap";
 import Clouds from "./components/svg/clouds";
 import College from "./components/svg/college";
 import LeftWall from "./components/svg/leftWall";
@@ -24,9 +23,15 @@ export default function Home() {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </Head>
-    <div id="app">
-      <h1 id="title">forecaste</h1>
-      <div id="gameContainer">
+    <div 
+      id="app" 
+      className="w-[400px] flex relative justify-center items-center flex-col bg-white border border-black">
+      <h1 
+        id="title"
+        className="font-sans text-blue-500">forecaste</h1>
+      <div 
+        id="gameContainer" 
+        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
         <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
         <College className="absolute top-[280.654px] left-[82.166px] right-[83.952px]"/>
         <LeftWall className="absolute top-[-2px] left-[-2px] z-10"/>
@@ -36,8 +41,14 @@ export default function Home() {
         <RightGate className="absolute top-[55.317px] right-[70.726px] origin-bottom-right"/>
         <Human className="absolute z-20 bottom-0 right-[152.923px] left-[120px]"/>
       </div>
-      <div id="viewport">
-        <button id="start-btn" onClick={startGame}>Start!</button>
+      <div 
+        className="z-[30] h-[650px] w-full flex items-center justify-center relative"
+        id="viewport">
+        <button 
+          className="z-30 font-sans text-lg pt-1 pr-2 pl-2 bg-black text-white rounded-md"
+          id="start-btn" 
+          onClick={startGame}>Start!
+        </button>
       </div>
     </div>
     </>
