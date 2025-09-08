442f2ccb6599c28ceaec3c7594cf2e0f351a8dbd
bijoy <bijoypjoseph07@gmail.com>
2025-09-08 15:37:01 +0530

    organize

---
+++ app/organize/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import BasicButton from "../components/functions/basicButton";
+import Clouds from "../components/svg/clouds";
+//import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+    <div 
+      id="app" 
+      className="w-[400px] flex relative justify-center items-center flex-col bg-white border border-black">
+      <h1 
+        id="title"
+        className="font-sans text-blue-500">forecaste</h1>
+      <div 
+        id="gameContainer" 
+        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
+            <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
+      </div>
+      <div 
+        className="z-[30] h-[650px] w-full flex flex-col items-start pb-3 justify-center relative p-5"
+        id="viewport">
+            <p>
+                You take a breath and a walk. And then - you text velivada circle.
+                By evening,you're in a crowded room of voices : some are trembling,some are raging strongly. 
+                One of the student shouts 'Enough is Enough'.
+                A Statement is drafted.Social media Campaign is started. 
+                You are very scared. But you are not alone.
+            </p>
+            <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
+                <li><BasicButton href="/mobilization" id="mobilization-btn" text="Mobilize." className="hover:bg-gray-800"/></li>
+                <li><BasicButton href="/" id="reset-btn" text="Reset Game" className="hover:bg-red-500"/></li>
+            </ul>
+      </div>
+    </div>
+    </>
+  );
+}
