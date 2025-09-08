a5b20a5079bed3a3483fc942ef6295f7ac38eb08
bijoy <bijoypjoseph07@gmail.com>
2025-09-08 12:33:19 +0530

    styling stuff

--- app/adjustment/page.tsx
+++ app/adjustment/page.tsx
@@ -16,13 +16,18 @@ export default function Home() {
         className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
       </div>
       <div 
-        className="z-[30] h-[650px] w-full flex items-end pb-3 justify-center relative"
+        className="z-[30] h-[650px] w-full flex items-center pb-3 justify-center relative p-5"
         id="viewport">
-        <div id="buttonContainer"
-          className="flex flex-row gap-3">
-          <BasicButton id="start-btn" text="Start!" className="hover:bg-gray-800"/>
-          <BasicButton id="reset-btn" text="Reset" className="hover:bg-red-500"/>
-        </div>
+            <p>
+                You are also in confusion and are going through a lot of cultural shocks. 
+                You've been facing microaggressions, othering and subtle casteism. 
+                What do you do? 
+                <ul className="flex flex-col gap-2 mt-2">
+                    <li><BasicButton id="isolate-btn" text="Isolate yourselves."/></li>
+                    <li><BasicButton id="smallBonds-btn" text="Form small bonds."/></li>
+                    <li><BasicButton id="dice-btn" text="Roll a dice." /></li>
+                </ul>
+            </p>
       </div>
     </div>
     </>
