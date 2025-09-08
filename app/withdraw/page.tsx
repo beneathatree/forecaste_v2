4ac86391f5350af9617738a63a16519628905741
bijoy <bijoypjoseph07@gmail.com>
2025-09-08 17:02:25 +0530

    it works

--- app/withdraw/page.tsx
+++ app/withdraw/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                 You drift from your friends. Your assignments first started coming late. Later, not at all.
             </p>
             <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
-                <li><BasicButton href="/consequence-and-fallout" id="withdraw-btn" text="Withdraw." className="hover:bg-gray-800"/></li>
+                <li><BasicButton href="/consequence-and-fallout" id="consequence-fallout-btn" text="Consequence and fallout." className="hover:bg-gray-800"/></li>
                 <li><BasicButton href="/" id="reset-btn" text="Reset Game" className="hover:bg-red-500"/></li>
             </ul>
       </div>
