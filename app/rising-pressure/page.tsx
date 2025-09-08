9b979a90fa6ab1b2f6a78914ad04dd6828c957c8
bijoy <bijoypjoseph07@gmail.com>
2025-09-08 15:07:52 +0530

    routine commit, i keep adding new pages, kindof lost track already

--- app/rising-pressure/page.tsx
+++ app/rising-pressure/page.tsx
@@ -22,12 +22,13 @@ export default function Home() {
         className="z-[30] h-[650px] w-full flex flex-col items-start pb-3 justify-center relative p-5"
         id="viewport">
             <p>
-                You have connected, and developed a friendship/small group of friends with other marginalized students and mentors.
-                You feel relief, trust, although you have a little cautious optimism. You introduce/get introduced to velivada circle.
-                Relationship system begins - you get mental health boost.
+                Deadlines loom. Syllabus piles up. 
+                The Classroom feels colder now. You participate less in classes, your questions hang unasked. 
+                You're pushing through late nights, but the gaps in guidance and belonging weigh you down. 
+                You try to keep up, but it feels like running uphill.
             </p>
             <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
-                <li><BasicButton href="/rising-pressure" id="pressure-btn" text="Rising pressure" className="hover:bg-gray-800"/></li>
+                <li><BasicButton href="inciting-incident" id="incident-btn" text="Inciting incident" className="hover:bg-gray-800"/></li>
                 <li><BasicButton href="/" id="reset-btn" text="Reset Game" className="hover:bg-red-500"/></li>
             </ul>
       </div>
