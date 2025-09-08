4e21802d2056a8511308eeb4220e9c4116386967
bijoy <bijoypjoseph07@gmail.com>
2025-09-08 16:45:12 +0530

    final page (for now)

---
+++ app/healing-with-fire/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import BasicButton from "../components/functions/basicButton";
+import Clouds from "../components/svg/clouds";
+//import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+    <div 
+      id="app" 
+      className="w-[400px] flex relative justify-center items-center flex-col bg-white border border-black">
+      <h1 
+        id="title"
+        className="font-sans text-blue-500">forecaste</h1>
+      <div 
+        id="gameContainer" 
+        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
+            <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
+      </div>
+      <div 
+        className="z-[30] h-[650px] w-full flex flex-col items-start pb-3 justify-center relative p-5"
+        id="viewport">
+            <p>
+                You decline the invitation to speak, instead you write something quietly, and publish on your anonymous blog.
+                You cook with your friends. You laugh again - you walk past the admin building, it no longer feels like a fortress. You didn't change the system. But just your story.
+                That's not a small thing.
+            </p>
+            <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
+                <li><BasicButton href="/" id="reset-btn" text="Reset Game" className="hover:bg-red-500"/></li>
+            </ul>
+      </div>
+    </div>
+    </>
+  );
+}
