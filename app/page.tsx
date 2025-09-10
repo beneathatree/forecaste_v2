e52dfa41b7cff6d1e8651e166ee3db2e89a1b44f
bijoy <bijoypjoseph07@gmail.com>
2025-09-10 10:09:26 +0530

    tring to reduce repetition of code

--- app/page.tsx
+++ app/page.tsx
@@ -35,8 +35,9 @@ export default function Home() {
       <h1 
         id="title"
         className="font-sans text-blue-500">forecaste</h1>
+    <div id="game-container">
       <div 
-        id="gameContainer" 
+        id="graphics-text-and-animations" 
         className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden ">
         <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
         <College className="absolute top-[280.654px] left-[82.166px] right-[83.952px]"/>
@@ -49,13 +50,14 @@ export default function Home() {
       </div>
       <div 
         className="z-[30] h-[650px] w-full flex items-end pb-3 justify-center relative"
-        id="viewport">
+        id="buttons-and-interactivity">
         <div id="buttonContainer"
           className="flex flex-row gap-3">
           <BasicButton id="start-btn" text="Start!" onClick={startGame} className="hover:bg-gray-800"/>
           <BasicButton id="reset-btn" text="Reset" onClick={resetGame} className="hover:bg-red-500"/>
         </div>
       </div>
+    </div>
     </div>
     </>
   );
