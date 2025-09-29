e7d9074192820daed785ed2399ab45b3c1866a2d
bijoy <bijoy.beneathatree@gmail.com>
2025-09-29 17:15:03 +0530

    refactor code

--- app/adjustment/page.tsx
+++ app/adjustment/page.tsx
@@ -1,11 +1,10 @@
-"use client"
+"use client";
 
 import BasicButton from "../components/functions/basicButton";
 import Clouds from "../components/svg/clouds";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-
   const router = useRouter();
 
   function handleCoinFlip() {
@@ -19,23 +18,42 @@ export default function Home() {
 
   return (
     <>
-      <div 
-        id="graphics-text-and-animations" 
-        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden ">
-            <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
+      <div
+        id="graphics-text-and-animations"
+        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden "
+      >
+        <Clouds className="absolute top-[50px] left-[40px] right-[30px]" />
       </div>
-      <div 
+      <div
         className="z-[30] h-full w-full flex flex-col items-start pb-3 justify-center relative p-5"
-        id="buttons-text-and-interactivity">
-            <p>
-                You are also in confusion and are going through a lot of cultural shocks. 
-                You have been facing microaggressions, othering and subtle casteism. 
-                What do you do? 
-            </p>
-            <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
-                <li><BasicButton href="/isolate" id="isolate-btn" text="Isolate yourselves." className="hover:bg-gray-800"/></li>
-                <li><BasicButton onClick={handleCoinFlip} id="smallBonds-btn" text="Form small bonds." className="hover:bg-gray-800"/></li>
-            </ul>
+        id="buttons-text-and-interactivity"
+      >
+        <p>
+          You are also in confusion and are going through a lot of cultural
+          shocks. You have been facing microaggressions, othering and subtle
+          casteism. What do you do?
+        </p>
+        <ul
+          className="w-full flex flex-col gap-2 mt-2 items-start"
+          id="possible-actions"
+        >
+          <li>
+            <BasicButton
+              href="/isolate"
+              id="isolate-btn"
+              text="Isolate yourselves."
+              className="hover:bg-gray-800"
+            />
+          </li>
+          <li>
+            <BasicButton
+              onClick={handleCoinFlip}
+              id="smallBonds-btn"
+              text="Form small bonds."
+              className="hover:bg-gray-800"
+            />
+          </li>
+        </ul>
       </div>
     </>
   );
