d11c67199721ffce77b52eb1fc1a13ea3d42fc9a
bijoy <bijoy.beneathatree@gmail.com>
2026-05-19 16:26:01 +0530

    /organize, /write/ /withdraw

--- app/withdraw/page.tsx
+++ app/withdraw/page.tsx
@@ -1,30 +1,32 @@
-"use client"
+"use client";
 
-import BasicButton from "../components/functions/basicButton";
-import Clouds from "../components/svg/unused/clouds";
-//import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { goBack } from "../components/functions/goBack";
+import { SceneTemplate, type Action } from "../components/page-templates";
 
 export default function Home() {
-  return (
-    <>
-      <div 
-        id="graphics-text-and-animations" 
-        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
-            <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
-      </div>
-      <div 
-        className="z-[30] h-[650px] w-full flex flex-col items-start pb-3 justify-center relative p-5"
-        id="buttons-text-and-interactivity">
-            <p>
-                You say nothing. You do nothing. 
-                You quietly log out of class, ignore the messages, skip the next few lectures.
-                You rehearse responses in your head at night, but speak none of them out loud.
-                You drift from your friends. Your assignments first started coming late. Later, not at all.
-            </p>
-            <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
-                <li><BasicButton href="/consequence-and-fallout" id="consequence-fallout-btn" text="Consequence and fallout." className="hover:bg-gray-800"/></li>
-            </ul>
-      </div>
-    </>
-  );
+    const router = useRouter();
+    const actions: Action[] = [
+        {
+            text: "Consequence and fallout.",
+            variant: "positive",
+            onClick: () => {
+                router.push("/consequence-and-fallout");
+            },
+        },
+        {
+            text: "Go back",
+            variant: "negative",
+            onClick: () => {
+                goBack(router);
+            },
+        },
+    ];
+
+    return (
+        <SceneTemplate
+            text="You say nothing. You do nothing. You quietly log out of class, ignore the messages, and skip the next few lectures. You rehearse responses in your head at night, but speak none of them out loud. You drift from your friends. Your assignments first started coming late. Later, not at all."
+            actions={actions}
+        ></SceneTemplate>
+    );
 }
--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,32 +1,32 @@
-"use client"
+"use client";
 
-import BasicButton from "../components/functions/basicButton";
-import Clouds from "../components/svg/unused/clouds";
-//import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { goBack } from "../components/functions/goBack";
+import { SceneTemplate, type Action } from "../components/page-templates";
 
 export default function Home() {
-  return (
-    <>
-      <div 
-        id="graphics-text-and-animations" 
-        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
-            <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
-      </div>
-      <div 
-        className="z-[30] h-[650px] w-full flex flex-col items-start pb-3 justify-center relative p-5"
-        id="buttons-text-and-interactivity">
-            <p>
-                You open a text document, and draft a blog post. Now, the words don&#39;t seem to be frozen.
-                You write about shame. About silence. How you&#39;re missing home. And how this is not new. How exhausted you feel pretending it doesn&#39;t hurt.
-                You post anonymously. It receives one like. And then two more. Acouple of comments.
-                &quot;This is important, please read.&quot;
-                &quot;Another crybaby.&quot;
-                You feel seen, but not entirely safe.
-            </p>
-            <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
-                <li><BasicButton href="/reflection-and-response" id="reflection-btn" text="Reflection and response." className="hover:bg-gray-800"/></li>
-            </ul>
-      </div>
-    </>
-  );
+    const router = useRouter();
+    const actions: Action[] = [
+        {
+            text: "Reflection and response.",
+            variant: "positive",
+            onClick: () => {
+                router.push("/reflection-and-response");
+            },
+        },
+        {
+            text: "Go back",
+            variant: "negative",
+            onClick: () => {
+                goBack(router);
+            },
+        },
+    ];
+
+    return (
+        <SceneTemplate
+            text={`You open a text document and draft a blog post. Now, the words don't seem to be frozen. You write about shame. About silence. How you're missing home. And how this is not new. How exhausted you feel pretending it doesn't hurt. You post anonymously. It receives one like. And then two more. A couple of comments. "This is important, please read." "Another crybaby." You feel seen, but not entirely safe.`}
+            actions={actions}
+        ></SceneTemplate>
+    );
 }
