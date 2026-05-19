42e3a9e4e66641df652294cc1a17bcbf693a718a
bijoy <bijoy.beneathatree@gmail.com>
2026-05-19 16:36:36 +0530

    /final-challenge-resolution and /internal-growth

--- app/internal-growth/page.tsx
+++ app/internal-growth/page.tsx
@@ -1,29 +1,32 @@
-"use client"
+"use client";
 
-import BasicButton from "../components/functions/basicButton";
-import Clouds from "../components/svg/unused/clouds";
-//import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { goBack } from "../components/functions/goBack";
+import { SceneTemplate, type Action } from "../components/page-templates";
 
 export default function Home() {
-  return (
-    <>
-      <div 
-        id="graphics-text-and-animations" 
-        className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
-            <Clouds className="absolute top-[50px] left-[40px] right-[30px]"/>
-      </div>
-      <div 
-        className="z-[30] h-[650px] w-full flex flex-col items-start pb-3 justify-center relative p-5"
-        id="buttons-text-and-interactivity">
-            <p>
-                Your body is tired. But one day you pick up a book you left halfway. 
-                Another day, you respond to that unread message. 
-                You start writing poetry again.
-            </p>
-            <ul className="w-full flex flex-col gap-2 mt-2 items-start" id="possible-actions">
-                <li><BasicButton href="/final-challenge-resolution" id="resolution-btn" text="Final challenge resolution." className="hover:bg-gray-800"/></li>
-            </ul>
-      </div>
-    </>
-  );
+    const router = useRouter();
+    const actions: Action[] = [
+        {
+            text: "Final challenge resolution.",
+            variant: "positive",
+            onClick: () => {
+                router.push("/final-challenge-resolution");
+            },
+        },
+        {
+            text: "Go back",
+            variant: "negative",
+            onClick: () => {
+                goBack(router);
+            },
+        },
+    ];
+
+    return (
+        <SceneTemplate
+            text="Your body is tired. But one day you pick up a book you left halfway. Another day, you respond to that unread message. You start writing poetry again."
+            actions={actions}
+        ></SceneTemplate>
+    );
 }
